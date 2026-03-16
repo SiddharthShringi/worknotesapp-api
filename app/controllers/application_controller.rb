ed55dcafe12550e942c57bac50f42f26e2ebda0c
Siddharth Shringi <siddharthshringi@gmail.com>
2026-03-16 18:30:03 +0530

    Added API tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
+  # use it later if required
+  # rescue_from ActiveRecord::RecordNotFound do
+  #   render json: { errors: [ "Resource not found" ] }, status: :not_found
+  # end
+
   protected
 
   def configure_permitted_parameters
